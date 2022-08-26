9f774b4d1c43b4e1242725263493376b03ef2134
2540709491 <102959142+2540709491@users.noreply.github.com>
2022-08-26 13:56:50 +0800

    2
    2

--- Oban/index.js
+++ Oban/index.js
@@ -1,14 +1,13 @@
-start()
-
+start();
 async function start() {
-    var names = await fetch("https://st1by.rth1.one/bannames.txt?" + new Date().getTime()).then(res => res.text())
+    var names = await fetch("https://st1by.rth1.one/bannames.txt?" + new Date().getTime()).then(res => res.text());
     console.log(names);
-    var link = names.split(/[(,)),]+/)
-    var box = document.getElementById("raws")
-    console.log(box.innerHTML)
-    box.innerHTML = "<div class=\"space\" name=\"space\"></div>"
+    var link = names.split(/[(\r\n)),\r\n]+/);
+    var box = document.getElementById("raws");
+    console.log(box.innerHTML);
+    box.innerHTML = "<div class=\"space\" name=\"space\"></div>";
     for (let index = 0; index < link.length; index++) {
-        box.innerHTML += "<h3 class=\"name\">" + link[index] + "</h3>"
+        box.innerHTML += "<h3 class=\"name\">" + link[index] + "</h3>";
     }
-    box.innerHTML += "<div class=\"space\" name=\"space\"></div>"
+    box.innerHTML += "<div class=\"space\" name=\"space\"></div>";
 }
